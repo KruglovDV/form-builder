d98a7e167731ffa717cc8a71ebc1116d40b5c29e
dimakruglov <kruglovdmitrij@itclover.ru>
2022-06-06 17:29:45 +0300

    adds form_for method with simple logic

--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -6,4 +6,10 @@ require_relative "form_builder/tag"
 module FormBuilder
   class Error < StandardError; end
   class UnknownTagError < Error; end
+
+  class << self
+    def from_for(entity, url: "#")
+      Tag.build "form", action: url, method: "post"
+    end
+  end
 end
--- test/lib/form_builder_test.rb
+++ test/lib/form_builder_test.rb
@@ -6,4 +6,19 @@ class FormBuilderTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FormBuilder::VERSION
   end
+
+  def test_form_builder
+    user = Struct.new(:name, :job, keyword_init: true).new name: "rob"
+
+    expected_empty_form = "<form action=\"#\" method=\"post\"></form>"
+    empty_form = FormBuilder.from_for user do |f|
+    end
+    puts empty_form
+    assert empty_form == expected_empty_form
+
+    expected_form_wit_url = "<form action=\"/users\" method=\"post\"></form>"
+    form_with_url = FormBuilder.from_for user, url: "/users" do |f|
+    end
+    assert form_with_url == expected_form_wit_url
+  end
 end
