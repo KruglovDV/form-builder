e6ff96b652c231af63a75af8f7b8bc64bbd7f44b
dimakruglov <kruglovdmitrij@itclover.ru>
2022-06-18 10:03:59 +0300

    adds submit name argument as default

--- lib/hexlet_code/form_schema.rb
+++ lib/hexlet_code/form_schema.rb
@@ -13,8 +13,10 @@ module HexletCode
       add_child({ type: :Input, attrs: { **attrs, name: name, value: get_value(name) } })
     end
 
-    def submit(**attrs)
-      add_child({ type: :Submit, attrs: { **attrs } })
+    def submit(name = '', **attrs)
+      prepared_attrs = { name: name, **attrs }
+      prepared_attrs = prepared_attrs.except(:name) if name.empty?
+      add_child({ type: :Submit, attrs: prepared_attrs })
     end
 
     private
